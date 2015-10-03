cf3ea207eb8171bb2b3a1f3ae69c2d9013683426
AJ <andrew_jones@outlook.com>
2015-10-03 18:07:19 +0100

    AJ: gps now working

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -6,18 +6,30 @@ var app = {
       document.getElementById('panicbtn').addEventListener('click', this.sendSms, false);
     },
     sendSms: function() {
-
-      var number = "+441982421043";
-      var message = "Hello from android";
-      var options = {
-          replaceLineBreaks: false, // true to replace \n by a new line, false by default
-          android: {
-              intent: '' // send SMS without open any other app
-          }
+      var gpsSuccess = function(position) {
+                var number = "+441982421043";
+                var message = "{'event': 'panic','long': '" + position.coords.longitude + "', 'lat': '" + position.coords.latitude + "'}";
+                var options = {
+                    replaceLineBreaks: false, // true to replace \n by a new line, false by default
+                    android: {
+                        intent: '' // send SMS without open any other app
+                    }
+                };
+                var success = function () { document.getElementById('panicmsg').innerHTML = 'Panic sent successfully'; };
+                var error = function (e) { document.getElementById('panicmsg').innerHTML = 'Panic Failed:' + e; };
+                sms.send(number, message, options, success, error);
       };
-      var success = function () { document.getElementById('panicmsg').innerHTML = 'Panic sent successfully'; };
-      var error = function (e) { document.getElementById('panicmsg').innerHTML = 'Panic Failed:' + e; };
-      sms.send(number, message, options, success, error);
+
+      var gpsFail = function (e) {
+        document.getElementById('panicmsg').innerHTML = 'Panic Failed:' + e;
+      };
+      var options = {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0
+      };
+      navigator.geolocation.getCurrentPosition(gpsFail, gpsFail, options);
+
       document.getElementById('panicmsg').setAttribute('style', 'display');
     }
 };
--- www/js/index.js
+++ www/js/index.js
@@ -3,26 +3,34 @@ var app = {
       this.bindEvents();
     },
     bindEvents: function() {
-      document.getElementById('panicbtn').addEventListener('click', this.panic, false);
+      document.getElementById('panicbtn').addEventListener('click', this.sendSms, false);
     },
-    setFailureText: function (e) { document.getElementById('panicmsg').innerHTML = 'Panic Failed:' + e; },
-    panic: function() {
-
-      var sendSms = function(position) {
-        var number = "+441982421043";
-        var message = "{'event': 'panic','long': '" + position.coords.longitude + "', 'lat': '" + position.coords.latitude + "'}";
-        var options = {
-          replaceLineBreaks: false, // true to replace \n by a new line, false by default
-          android: {
-            intent: '' // send SMS without open any other app
-          }
-        };
-        var success = function () { document.getElementById('panicmsg').innerHTML = 'Panic sent successfully'; };
-        sms.send(number, message, options, success, setFailureText);
+    sendSms: function() {
+      var gpsSuccess = function(position) {
+                var number = "+441982421043";
+                var message = "{'event': 'panic','long': '" + position.coords.longitude + "', 'lat': '" + position.coords.latitude + "'}";
+                var options = {
+                    replaceLineBreaks: false, // true to replace \n by a new line, false by default
+                    android: {
+                        intent: '' // send SMS without open any other app
+                    }
+                };
+                var success = function () { document.getElementById('panicmsg').innerHTML = 'Panic sent successfully'; };
+                var error = function (e) { document.getElementById('panicmsg').innerHTML = 'Panic Failed:' + e; };
+                sms.send(number, message, options, success, error);
       };
 
-      navigator.geolocation.getCurrentPosition(sendSms,
-                                               setFailureText);
+      var gpsFail = function (e) {
+        document.getElementById('panicmsg').innerHTML = 'Panic Failed:' + e;
+      };
+      
+      var options = {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0
+      };
+
+      navigator.geolocation.getCurrentPosition(gpsSuccess, gpsFail, options);
 
       document.getElementById('panicmsg').setAttribute('style', 'display');
     }
