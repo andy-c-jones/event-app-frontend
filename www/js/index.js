14d4d49e7cd07681ea9bc23d4236abe4f253c75f
AJ <andrew_jones@outlook.com>
2015-10-03 17:04:30 +0100

    AJ: app now sends SMS

--- www/js/index.js
+++ www/js/index.js
@@ -3,11 +3,23 @@ var app = {
       this.bindEvents();
     },
     bindEvents: function() {
-        document.getElementById('panicbtn').addEventListener('click', this.receivedEvent, false);
+      document.getElementById('panicbtn').addEventListener('click', this.sendSms, false);
     },
-    receivedEvent: function() {
-     document.getElementById('panicmsg').setAttribute('style', 'display');
- }
+    sendSms: function() {
+
+      var number = "+441982421043";
+      var message = "Hello from android";
+      var options = {
+          replaceLineBreaks: false, // true to replace \n by a new line, false by default
+          android: {
+              intent: '' // send SMS without open any other app
+          }
+      };
+      var success = function () { document.getElementById('panicmsg').innerHTML = 'Panic sent successfully'; };
+      var error = function (e) { document.getElementById('panicmsg').innerHTML = 'Panic Failed:' + e; };
+      sms.send(number, message, options, success, error);
+      document.getElementById('panicmsg').setAttribute('style', 'display');
+    }
 };
 
 app.initialize();
