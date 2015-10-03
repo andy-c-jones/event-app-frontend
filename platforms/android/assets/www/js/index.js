353bc838145c82e31aef24f328b12aa3b59fcaeb
AJ <andrew_jones@outlook.com>
2015-10-03 19:02:43 +0100

    AJ: improved UI

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -7,28 +7,30 @@ var app = {
     },
     sendSms: function() {
       var gpsSuccess = function(position) {
-                var number = "+441982421043";
-                var message = "{'event': 'panic','long': '" + position.coords.longitude + "', 'lat': '" + position.coords.latitude + "'}";
-                var options = {
-                    replaceLineBreaks: false, // true to replace \n by a new line, false by default
-                    android: {
-                        intent: '' // send SMS without open any other app
-                    }
-                };
-                var success = function () { document.getElementById('panicmsg').innerHTML = 'Panic sent successfully'; };
-                var error = function (e) { document.getElementById('panicmsg').innerHTML = 'Panic Failed:' + e; };
-                sms.send(number, message, options, success, error);
+        var number = "+441982421043";
+        var message = "{'event': 'panic','long': '" + position.coords.longitude + "', 'lat': '" + position.coords.latitude + "'}";
+        var options = {
+        replaceLineBreaks: false, // true to replace \n by a new line, false by default
+          android: {
+            intent: '' // send SMS without open any other app
+          }
+        };
+        var success = function () { document.getElementById('panicmsg').innerHTML = 'Panic sent successfully'; };
+        var error = function (e) { document.getElementById('panicmsg').innerHTML = 'Panic Failed:' + e; };
+        sms.send(number, message, options, success, error);
       };
 
       var gpsFail = function (e) {
         document.getElementById('panicmsg').innerHTML = 'Panic Failed:' + e;
       };
+
       var options = {
         enableHighAccuracy: true,
         timeout: 5000,
         maximumAge: 0
       };
-      navigator.geolocation.getCurrentPosition(gpsFail, gpsFail, options);
+
+      navigator.geolocation.getCurrentPosition(gpsSuccess, gpsFail, options);
 
       document.getElementById('panicmsg').setAttribute('style', 'display');
     }
