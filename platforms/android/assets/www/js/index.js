19634a5a16cb8dc228bb94963ac419b5fb3ef415
AJ <andrew_jones@outlook.com>
2015-10-03 21:28:35 +0100

    AJ: added reg

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -1,20 +1,21 @@
 var app = {
+    number: "+441793250159",
+    options: {
+      replaceLineBreaks: false, // true to replace \n by a new line, false by default
+        android: {
+          intent: '' // send SMS without open any other app
+        }
+    },
     initialize: function() {
       this.bindEvents();
     },
     bindEvents: function() {
       document.getElementById('panicbtn').addEventListener('click', this.sendSms, false);
+      document.getElementById('register').addEventListener('click', this.register, false);
     },
     sendSms: function() {
       var gpsSuccess = function(position) {
-        var number = "+441793250159";
         var message = "{'event': 'panic','long': '" + position.coords.longitude + "', 'lat': '" + position.coords.latitude + "'}";
-        var options = {
-        replaceLineBreaks: false, // true to replace \n by a new line, false by default
-          android: {
-            intent: '' // send SMS without open any other app
-          }
-        };
         var success = function () { document.getElementById('panicmsg').innerHTML = 'Panic sent successfully'; };
         var error = function (e) { document.getElementById('panicmsg').innerHTML = 'Panic Failed:' + e; };
         sms.send(number, message, options, success, error);
@@ -24,15 +25,27 @@ var app = {
         document.getElementById('panicmsg').innerHTML = 'Panic Failed:' + e;
       };
 
-      var options = {
+      var gpsOptions = {
         enableHighAccuracy: true,
         timeout: 5000,
         maximumAge: 0
       };
 
-      navigator.geolocation.getCurrentPosition(gpsSuccess, gpsFail, options);
+      navigator.geolocation.getCurrentPosition(gpsSuccess, gpsFail, gpsOptions);
 
       document.getElementById('panicmsg').setAttribute('style', 'display');
+    },
+    register: function() {
+      var regMessage = "{'event': 'register','name': '" + document.getElementById('name').value + "', 'nok': '" + document.getElementById('nok').value + "'}";
+      var regSuccess = function () {
+        document.getElementById('regmsg').innerHTML = 'Successfully registered';
+        document.getElementById('register-form').setAttribute('hidden');
+        document.getElementById('panic-form').removeAttribute("hidden");
+      };
+      var regError = function (e) {
+        document.getElementById('regmsg').innerHTML = 'Register failed:' + e;
+       };
+      sms.send(number, regMessage, options, regSuccess, regSuccess);
     }
 };
 
